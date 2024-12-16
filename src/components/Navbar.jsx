e130e11c5c2acf8ac83de9f6f3045db3f771e912
Mayank-p19 <mayankpunia74@gmail.com>
2024-12-16 18:55:26 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src="logo_dark.svg" alt="React logo"/>
+                    <span>ReactFacts</span>
+                </nav>
+            </header>
+        </>
+    )
+}
